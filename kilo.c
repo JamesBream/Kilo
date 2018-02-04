21dd990f5c9d6093695e0a935613d570f81018f5
James Bream <JamesBream@users.noreply.github.com>
2018-02-04 20:47:10 +0000

    Read from stdin

--- kilo.c
+++ kilo.c
@@ -1,3 +1,8 @@
+#include <unistd.h>
+
 int main() {
+    char c;
+    /* Read byte(s) from stdin into c, until read() returns 0 (EOF) or q key is pressed */
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
