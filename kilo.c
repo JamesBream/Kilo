b26fba9e8be58c5a62ce390d597bf4c500a49b80
James Bream <JamesBream@users.noreply.github.com>
2018-02-07 15:33:28 +0000

    Restore user's terminal attributes on exit

--- kilo.c
+++ kilo.c
@@ -1,11 +1,23 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/* Original copy of terminal attributes */
+struct termios orig_termios;
+
+void disableRawMode() {
+    /* Restore original terminal attributes on exit */
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-    struct termios raw;
     /* Read in terminal attributes */
-    tcgetattr(STDIN_FILENO, &raw);
-    /* Disable echo - bit flip */ 
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    /* Register restore function */
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    /* Disable echo on copy of attributes struct - bit flip (╯°□°）╯︵ ┻━┻ */ 
     raw.c_lflag &= ~(ECHO);
     /* Set new terminal attributes */
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
