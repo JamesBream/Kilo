4d475a9c0c1dee6f7ca2f0ec13f1f56f255ba565
James Bream <JamesBream@users.noreply.github.com>
2018-02-08 16:34:36 +0000

    Implement append buffer to avoid excessive writing to screen

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -114,29 +115,60 @@ int getWindowSize(int *rows, int *cols) {
     }
 }
 
+/*** Append Buffer ***/
+
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+    /* Get block of memory equivalent to existing string plus appended item */ 
+    char *new = realloc(ab->b, ab->len + len);
+
+    if (new == NULL) return;
+    /* Copy s to the end of new buffer */
+    memcpy(&new[ab->len], s, len);
+    /* Update pointers */
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    /* Free memory */
+    free(ab->b);
+}
+
 /*** Output ***/
 
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
     int y;
     /* Loop to draw row tildes */
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~", 1);
+        abAppend(ab, "~", 1);
 
         if (y < E.screenrows - 1) {
-            write(STDOUT_FILENO, "\r\n", 2);
+            abAppend(ab, "\r\n", 2);
         }
     }
 }
 
 void editorRefreshScreen() {
+    struct abuf ab = ABUF_INIT;
+
     /* Use VT100 Erase in Display (2J) to clear screen */
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    /* VT100 Cursor Position */
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[2J", 4);
+    /* VT100  Reset Cursor Position */
+    abAppend(&ab, "\x1b[H", 3);
+    /* Draw rows to buffer */
+    editorDrawRows(&ab);
 
-    editorDrawRows();
-
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[H", 3);
+    /* Write buffer to screen then free its memory */
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
 }
 
 /*** Input ***/
