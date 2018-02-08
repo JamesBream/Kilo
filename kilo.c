4af9645f36cd0a5341f80d06a83d7f2b3854da1f
James Bream <JamesBream@users.noreply.github.com>
2018-02-08 15:24:39 +0000

    Fix last line tilde not printing

--- kilo.c
+++ kilo.c
@@ -120,7 +120,11 @@ void editorDrawRows() {
     int y;
     /* Loop to draw row tildes */
     for (y = 0; y < E.screenrows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        write(STDOUT_FILENO, "~", 1);
+
+        if (y < E.screenrows - 1) {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
     }
 }
 
